505698b49c84366d29221ee43857e3a5ca0ce74a
Bernhard Schiffer <schiffer.be@gmail.com>
2020-03-25 23:11:19 +0100

    change data type for hash field

--- db_init/init_user_table.sql
+++ db_init/init_user_table.sql
@@ -2,7 +2,7 @@ create table users(
 	id serial not null,
     email varchar(255) unique not null,
 	name varchar(255) not null,
-    hash varchar(255) not null,
+    hash text not null,
 	roles text[] not null,
 	primary key (id)
 );
