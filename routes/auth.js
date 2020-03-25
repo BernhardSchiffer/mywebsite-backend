7d9da135805dee33b59a57fd39bd9f655fe62504
Bernhard Schiffer <schiffer.be@gmail.com>
2020-03-25 23:12:51 +0100

    change from mongodb to postgres

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const { User } = require('../models/user');
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('../db')
 const bcrypt = require('bcryptjs');
 const Joi = require('joi');
 const _ = require('lodash');
 const router = express.Router();
+
+const selectStatment = 'select email, name, hash, roles from users where email = $1;'
 
 //Anmeldung eines Benutzers
 router.post('/', async (req, res) => {
@@ -13,10 +15,7 @@ router.post('/', async (req, res) => {
       return res.status(400).send(error.details[0].message);
    }
 
-   let user = await User.findOne({ email: req.body.email });
-   if (!user) {
-      return res.status(400).send('Invalid email or password.');
-   }
+   const user = await getUser(req.body.email);
 
    const validPassword = await bcrypt.compare(req.body.password, user.password);
    if (!validPassword) {
@@ -27,6 +26,20 @@ router.post('/', async (req, res) => {
 
    res.header('x-auth-token', token).send(_.pick(user, ['name', 'email']));
 });
+
+async function getUser(email) {
+
+   try {
+      const res = await db.query(selectStatment, [email]);
+      const user = await new User(res.rows[0].email, res.rows[0].name, res.rows[0].hash, res.rows[0].roles);
+      return user;
+   }
+   catch(err) {
+      console.error(err);
+      return res.status(400).send('Invalid email or password.');
+   }
+
+}
 
 function validate(req) {
    const schema =
